6b4b07d92ac7a4b277ee81da8794bb7d01633a47
JeronimoUnigarro <133828381+JeronimoUnigarro@users.noreply.github.com>
2024-10-29 17:22:16 -0500

    app.py

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ class CircularList:
         self.data = data
     
     def get(self, index):
-        # Retorna el elemento en una posición, haciendo que la lista sea circular
+      
         return self.data[index % len(self.data)]
 
 class Clock:
     def __init__(self):
-        # Círculo de números en el reloj (1 a 12)
+     
         self.numbers = CircularList([str(i) for i in range(1, 13)])
 
     def get_current_time(self):
         now = datetime.datetime.now()
-        hour = now.hour % 12  # Convertimos a formato de 12 horas
+        hour = now.hour % 12  
         minute = now.minute
         second = now.second
         return hour, minute, second
